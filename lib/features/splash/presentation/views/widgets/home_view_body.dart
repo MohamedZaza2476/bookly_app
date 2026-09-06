34fba50eb3e5df34c3f2e84da04ca51f600a7fa2
MohamedZaza2476 <mzaza2476@gmail.com>
2026-09-07 00:08:49 +0300

    add fonts family & best seller list view item part one

--- lib/features/splash/presentation/views/widgets/home_view_body.dart
+++ lib/features/splash/presentation/views/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/core/utils/assets.dart';
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:bookly_app/features/splash/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly_app/features/splash/presentation/views/widgets/featured_list_view.dart';
@@ -17,6 +18,34 @@ class HomeViewBody extends StatelessWidget {
           FeaturedBooksListView(),
           SizedBox(height: 50),
           Text('Best Seller', style: Styles.titleMedium),
+          BestSellerListViewItem(),
+        ],
+      ),
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 125,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.5 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                image: const DecorationImage(
+                  image: AssetImage(AssetsData.testImage),
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
