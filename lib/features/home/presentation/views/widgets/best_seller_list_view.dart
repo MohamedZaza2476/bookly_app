9fc6596f2cae8348ab8e033928f87fc4b56fe97c
MohamedZaza2476 <mzaza2476@gmail.com>
2026-09-10 01:02:06 +0300

    create book details view

--- lib/features/home/presentation/views/widgets/best_seller_list_view.dart
+++ lib/features/home/presentation/views/widgets/best_seller_list_view.dart
@@ -1,4 +1,4 @@
-import 'package:bookly_app/features/splash/presentation/views/widgets/best_seller_list_view_item.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/best_seller_list_view_item.dart';
 import 'package:flutter/widgets.dart';
 
 class BestSellerListView extends StatelessWidget {
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,7 +1,8 @@
 import 'package:bookly_app/core/utils/styles.dart';
-import 'package:bookly_app/features/splash/presentation/views/widgets/best_seller_list_view.dart';
-import 'package:bookly_app/features/splash/presentation/views/widgets/custom_app_bar.dart';
-import 'package:bookly_app/features/splash/presentation/views/widgets/featured_list_view.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/best_seller_list_view.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/custom_app_bar.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/featured_list_view.dart';
+
 import 'package:flutter/material.dart';
 
 class HomeViewBody extends StatelessWidget {
