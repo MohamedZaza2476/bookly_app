1119927c6ec28100536ea6cf245100d81513f001
MohamedZaza2476 <mzaza2476@gmail.com>
2026-09-15 00:42:14 +0300

    create custom book details app bar

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:bookly_app/constants.dart';
+import 'package:bookly_app/core/utils/app_router.dart';
 import 'package:bookly_app/core/utils/assets.dart';
 import 'package:bookly_app/features/home/presentation/views/home_view.dart';
 import 'package:bookly_app/features/splash/presentation/views/widgets/sliding_text.dart';
@@ -53,7 +54,7 @@ class _SplashViewBodyState extends State<SplashViewBody>
       //   duration: kTransitionDuration,
       // );
 
-      GoRouter.of(context).push('/homeView');
+      GoRouter.of(context).push(AppRouter.kHomeView);
     });
   }
 
